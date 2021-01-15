fc372a1b0d4cc5635139a215ba31ed4aa5909290
Udit Kapoor <ukapoor.29@gmail.com>
2021-01-15 22:45:10 +0530

    tweaked for heroku

--- image_repository/settings.py
+++ image_repository/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'image_repository.urls'
@@ -123,3 +127,7 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_STORAGE =  'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
