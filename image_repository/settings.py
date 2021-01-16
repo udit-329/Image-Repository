a80c81388ef9c8f9ef28d9c9c1605d32be733de6
Udit Kapoor <ukapoor.29@gmail.com>
2021-01-16 23:48:45 +0530

    db

--- image_repository/settings.py
+++ image_repository/settings.py
@@ -81,13 +81,18 @@ WSGI_APPLICATION = 'image_repository.wsgi.application'
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 
-
+'''
 DATABASES = {
       'default': dj_database_url.config(
           default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
       )
   }
+'''
+DATABASES = {
+'default': {
+    'ENGINE': 'django.db.backends.postgresql',
 
+}
 DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
