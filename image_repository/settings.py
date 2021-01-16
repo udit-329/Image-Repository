457e846c6428fbbdc113a597dc3a7eb3710919dd
Udit Kapoor <ukapoor.29@gmail.com>
2021-01-16 23:06:59 +0530

    heroku

--- image_repository/settings.py
+++ image_repository/settings.py
@@ -80,15 +80,15 @@ WSGI_APPLICATION = 'image_repository.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-'''
+
+
 DATABASES = {
       'default': dj_database_url.config(
           default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
       )
   }
-'''
 
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
